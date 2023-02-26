e73fdd4d96adec448a452cf1d5a8748be2eca3fa
Felipe Santana <felipe.jesus0801@gmail.com>
2023-02-26 17:11:33 -0300

    create down part

--- src/components/Smartphone/index.tsx
+++ src/components/Smartphone/index.tsx
@@ -1,9 +1,47 @@
 import './styles.scss'
+import LeftArrow from '../../assets/arrow-left-2.svg'
+import ImageCell from '../../assets/image-smartphone.png'
+import Add from '../../assets/add.svg'
+
+import Map from '../../assets/map.svg'
+import TruckTime from '../../assets/truck-time.svg'
 
 export function Smartphone(){
   return(
     <div id='cell'>
-      Cell
+      <div className='header-smartphone'>
+        <img src={LeftArrow} alt="Flecha Esquerda" width="8px"/> 
+        <span>Check Out</span>
+      </div>
+     <div className='menu-smartphone'> 
+        <ul>
+          <li>Produtos</li>
+          <li>Carrinho</li>
+        </ul>
+     </div>
+    <div className='main-content'>
+      <img src={ImageCell} alt="Image Cell" />
+
+      <div className='add-cart'>
+        <img src={Add} alt="" />
+        Adicionar no Carrinho
+      </div>
+
+      <div className='delivery-smartphone'>
+
+        <div className='point-info'>
+          <div className='block address'><img src={Map} alt="" /></div>
+          <span>10506 - 4904 Deans Lane - Bedford Village	- New York - US</span>
+        </div>
+        
+        <div className='point-info'>
+          <div className='block date'><img src={TruckTime} alt="" /></div>
+          <span>Quinta-feira - 13:00</span>
+        </div>
+        
+      </div>
+    </div>
+
     </div>
   )
 }
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -6,50 +6,134 @@ import ClickBus from '../../assets/clickbus.svg'
 import Moto from '../../assets/motorola.svg'
 import Levis from '../../assets/levis.svg'
 
+import Ellipse1 from '../../assets/ellipse-photo1.png'
+import Ellipse2 from '../../assets/ellipse-photo2.png'
+import Ellipse3 from '../../assets/ellipse-photo3.png'
+import Coffee from '../../assets/coffee.svg'
+import Security from '../../assets/security.svg'
+import Simple from '../../assets/simple.svg'
+
+
 import { HeaderMenu } from '../../components/HeaderMenu'
 import { Smartphone } from '../../components/Smartphone'
 import { ButtonCreateAccount } from '../../components/ButtonCreateAccount'
 
 
-export function Home(){
+export function Home() {
 
-  return(
+  return (
     <div>
       <div id="background-top">
         <HeaderMenu />
-
         <main>
-          <div className='base-title'>
-            <div className='title-highlight'>
-              <h5>VIVA O PAGAMENTO INSTANTÂNEO</h5>
-              <h1>O melhor gateway de pagamentos <span>instantâneos</span> do Brasil</h1>
-              <h5>Comece a vender na internet ou em seu ponto de venda com velocidade, segurança para você conseguir crescer seu negócio</h5>
+          <div id="top-header">
+            <div className='base-title'>
+              <div className='title-highlight'>
+                <h5>VIVA O PAGAMENTO INSTANTÂNEO</h5>
+                <h1>O melhor gateway de pagamentos <span>instantâneos</span> do Brasil</h1>
+                <h5>Comece a vender na internet ou em seu ponto de venda com velocidade, segurança para você conseguir crescer seu negócio</h5>
+
+              </div>
+
+              <div className='about-acess'>
+                <ButtonCreateAccount type="blue" />
+              </div>
+
+              <div className='info-accept-qty'>
+                <div className='ellipses'>
+                  <img src={Ellipse1} alt="" />
+                  <img src={Ellipse2} alt="" />
+                  <img src={Ellipse3} alt="" />
+                </div>
+                <span>+250.000 pessoas aprovam a PayMee</span>
+              </div>
 
             </div>
-
-            <div className='about-acess'>
-              <ButtonCreateAccount type="blue"/>
+            <div className="base-cell">
+              <Smartphone />
             </div>
-
-            <div className='info-accept-qty'>
-              Teste
-            </div>
-            
           </div>
-          <div className="base-cell">
-            <Smartphone />
+          <div id='down-part'>
+            <div id='stamp-container'>
+              <img src={Mobbly} alt="" />
+              <img src={Maxmi} alt="" />
+              <img src={ClickBus} alt="" />
+              <img src={Moto} alt="" />
+              <img src={Levis} alt="" />
+            </div>
+            <div id='about-paymee'>
+              <div className='description-about'>
+                <h5>Porque a Paymee?</h5>
+                <p>Faça parte da revolução dos pagamentos instantâneos.</p>
+                <span>
+                  Oferecemos soluções de pagamento instantâneas para você<br />
+                  conseguir elevar seu negócio para outro patamar.
+                </span>
+              </div>
+              <div className='info-relevant-blocks'>
+                <div className="block simple">
+                  <img src={Simple} alt="" />
+                  <p><span> | </span> Simples</p>
+                  <span>
+                    Lorem ipsum dolor sit amet, 
+                    consectetur adipiscing elit, 
+                    sed do eiusmod tempor.
+
+                    Lorem ipsum dolor sit amet, 
+                    consectetur adipiscing elit, 
+                    sed do eiusmod tempor 
+                  </span>
+                </div>
+                <div className="block security">
+                  <img src={Security} alt="" />
+                  <p><span> | </span> Seguro</p>
+                  <span>
+                    Lorem ipsum dolor sit amet, 
+                    consectetur adipiscing elit, 
+                    sed do eiusmod tempor.
+
+                    Lorem ipsum dolor sit amet, 
+                    consectetur adipiscing elit, 
+                    sed do eiusmod tempor 
+                  </span>
+                </div>
+                <div className="block snapshot">
+                  <img src={Coffee} alt="" />
+                  <p><span> | </span>Instantâneo</p>
+                  <span>
+                    Lorem ipsum dolor sit amet, 
+                    consectetur adipiscing elit, 
+                    sed do eiusmod tempor.
+
+                    Lorem ipsum dolor sit amet, 
+                    consectetur adipiscing elit, 
+                    sed do eiusmod tempor 
+                  </span>
+                </div>
+                <div className="block security">
+                  <img src={Security} alt="" />
+                  <p><span> | </span> Seguro</p>
+                  <span>
+                    Lorem ipsum dolor sit amet, 
+                    consectetur adipiscing elit, 
+                    sed do eiusmod tempor.
+
+                    Lorem ipsum dolor sit amet, 
+                    consectetur adipiscing elit, 
+                    sed do eiusmod tempor 
+                  </span>
+                </div>
+              </div>
+
+
+              
+            </div>
           </div>
+
+
         </main>
-        <div id='stamp-container'>
-          <img src={Mobbly} alt="" />              
-          <img src={Maxmi} alt="" />
-          <img src={ClickBus} alt="" />
-          <img src={Moto} alt="" />
-          <img src={Levis} alt="" />
-        </div>
-        
       </div>
     </div>
-    
+
   )
 }
