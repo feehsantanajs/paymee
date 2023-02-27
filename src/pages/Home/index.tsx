49cf7d9c5c577d6116655b08ead04b8f16bf38a3
Felipe Santana <felipe.jesus0801@gmail.com>
2023-02-26 22:02:28 -0300

    update code home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -12,6 +12,13 @@ import Ellipse3 from '../../assets/ellipse-photo3.png'
 import Coffee from '../../assets/coffee.svg'
 import Security from '../../assets/security.svg'
 import Simple from '../../assets/simple.svg'
+import CodeImg from '../../assets/code.png'
+import RightArrow from '../../assets/right-icon.svg'
+
+import BoxIcon from '../../assets/icons/box-icon.svg'
+import ChainIcon from '../../assets/icons/chain-icon.svg'
+import PhoneIcon from '../../assets/icons/phone-icon.svg'
+import SquadIcon from '../../assets/icons/squad-deshed-icon.svg'
 
 
 import { HeaderMenu } from '../../components/HeaderMenu'
@@ -22,10 +29,11 @@ import { ButtonCreateAccount } from '../../components/ButtonCreateAccount'
 export function Home() {
 
   return (
-    <div>
+    <div id='container'>
+
       <div id="background-top">
-        <HeaderMenu />
-        <main>
+        <div className='content-top'>
+          <HeaderMenu />
           <div id="top-header">
             <div className='base-title'>
               <div className='title-highlight'>
@@ -53,15 +61,22 @@ export function Home() {
               <Smartphone />
             </div>
           </div>
-          <div id='down-part'>
-            <div id='stamp-container'>
+        </div>
+      </div>
+      <main>
+        <div id='down-part'>
+          <div className='stamp-container'>
+            <div className='content-stamp-img'>
               <img src={Mobbly} alt="" />
               <img src={Maxmi} alt="" />
               <img src={ClickBus} alt="" />
               <img src={Moto} alt="" />
               <img src={Levis} alt="" />
             </div>
-            <div id='about-paymee'>
+
+          </div>
+          <div id='about-paymee'>
+            <div className='content-about-paymee'>
               <div className='description-about'>
                 <h5>Porque a Paymee?</h5>
                 <p>Faça parte da revolução dos pagamentos instantâneos.</p>
@@ -75,64 +90,134 @@ export function Home() {
                   <img src={Simple} alt="" />
                   <p><span> | </span> Simples</p>
                   <span>
-                    Lorem ipsum dolor sit amet, 
-                    consectetur adipiscing elit, 
+                    Lorem ipsum dolor sit amet,
+                    consectetur adipiscing elit,
                     sed do eiusmod tempor.
 
-                    Lorem ipsum dolor sit amet, 
-                    consectetur adipiscing elit, 
-                    sed do eiusmod tempor 
+                    Lorem ipsum dolor sit amet,
+                    consectetur adipiscing elit,
+                    sed do eiusmod tempor
                   </span>
                 </div>
                 <div className="block security">
                   <img src={Security} alt="" />
                   <p><span> | </span> Seguro</p>
                   <span>
-                    Lorem ipsum dolor sit amet, 
-                    consectetur adipiscing elit, 
+                    Lorem ipsum dolor sit amet,
+                    consectetur adipiscing elit,
                     sed do eiusmod tempor.
 
-                    Lorem ipsum dolor sit amet, 
-                    consectetur adipiscing elit, 
-                    sed do eiusmod tempor 
+                    Lorem ipsum dolor sit amet,
+                    consectetur adipiscing elit,
+                    sed do eiusmod tempor
                   </span>
                 </div>
                 <div className="block snapshot">
                   <img src={Coffee} alt="" />
                   <p><span> | </span>Instantâneo</p>
                   <span>
-                    Lorem ipsum dolor sit amet, 
-                    consectetur adipiscing elit, 
+                    Lorem ipsum dolor sit amet,
+                    consectetur adipiscing elit,
                     sed do eiusmod tempor.
 
-                    Lorem ipsum dolor sit amet, 
-                    consectetur adipiscing elit, 
-                    sed do eiusmod tempor 
+                    Lorem ipsum dolor sit amet,
+                    consectetur adipiscing elit,
+                    sed do eiusmod tempor
                   </span>
                 </div>
                 <div className="block security">
                   <img src={Security} alt="" />
                   <p><span> | </span> Seguro</p>
                   <span>
-                    Lorem ipsum dolor sit amet, 
-                    consectetur adipiscing elit, 
+                    Lorem ipsum dolor sit amet,
+                    consectetur adipiscing elit,
                     sed do eiusmod tempor.
 
-                    Lorem ipsum dolor sit amet, 
-                    consectetur adipiscing elit, 
-                    sed do eiusmod tempor 
+                    Lorem ipsum dolor sit amet,
+                    consectetur adipiscing elit,
+                    sed do eiusmod tempor
                   </span>
                 </div>
               </div>
-
-
-              
+            </div>
+          </div>
+          <div id="how-it-works">
+            <div className='content'>
+              <img src={CodeImg} alt="imagem do codigo" />
+              <div className='description-how-it-works'>
+                <h5>COMO FUNCIONA</h5>
+                <h1>
+                  API poderosa para você <br />
+                  expandir e aprimorar <br />
+                  seu negócio
+                </h1>
+                <p>
+                  Oferecemos soluções de pagamento instantâneas para<br /> você conseguir elevar seu negócio para outro
+                </p>
+                <ButtonCreateAccount type='orange' />
+                <span>Desenvolvedores <img src={RightArrow} alt="" /></span>
+              </div>
             </div>
           </div>
 
+          <div id="about-way-payment">
+            <div className='content-way-payemnt'>
+              <div className='solutions'>
+                <h5>Soluções</h5>
+                <h1>A maneira mais fácil de  <br />receber pagamentos online.</h1>
+                <p>Oferecemos soluções de pagamento instantâneas para você conseguir elevar seu negócio para outro</p>
+                <ButtonCreateAccount type='orange' />
+              </div>
+              <div className="options-way-payment">
+                <div className="scrap">
+                  <div className="block link">
+                    <img src={ChainIcon} alt="Corrente Icone" />
+                    <p> Link de pagamento</p>
+                    <span>
+                      Oferecemos soluções de pagamento instantâneas para você conseguir elevar seu negócio para outro
+                    </span>
+                  </div>
+                  <div className="block app">
+                    <img src={PhoneIcon} alt="Celular icone" />
+                    <p> APP da PayMee</p>
+                    <span>
+                      Oferecemos soluções de pagamento instantâneas para você conseguir elevar seu negócio para outro
+                    </span>
+                  </div>
+                </div>
+                <div className="scrap">
+                  <div className="block pix">
+                    <img src={SquadIcon} alt="Quadrado Icone" />
+                    <p> Simples</p>
+                    <span>
+                    Oferecemos soluções de pagamento instantâneas para você conseguir elevar seu negócio para outro
+                    </span>
+                  </div>
+                  <div className="block eccomerce">
+                    <img src={BoxIcon} alt="Caixa Icone" />
+                    <p>Simples</p>
+                    <span>
+                    Oferecemos soluções de pagamento instantâneas para você conseguir elevar seu negócio para outro
+                    </span>
+                  </div>
+                </div>
+              </div>
+            </div>
 
-        </main>
-      </div>
+          </div>
+
+          <div id="integrations-apps">
+            <div className="content-integrations-apps">
+              <div className="brands"></div>
+              <div className="description-about-integrations">
+                <h1>Integrações com as principais plataformas</h1>
+                <p>Integre-se a diferentes plataformas de ecommerce
+para aprimorar e melhorar seu negócio</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
     </div>
 
   )
