67312d4522bf81e8ad32b83b26ba457ccf7444dd
Felipe Santana <felipe.jesus0801@gmail.com>
2023-03-03 03:06:47 -0300

    updated support page

--- src/pages/Support/index.tsx
+++ src/pages/Support/index.tsx
@@ -1,15 +1,94 @@
-import { HeaderMenu } from '../../components/HeaderMenu'
+
 import './styles.scss'
-export function Support(){
+
+import SearchIcon from '../../assets/icons/search-icon.svg'
+import EmailIcon from '../../assets/icons/email-icon.svg'
+
+import { FooterContainer } from '../../components/FooterContainer'
+import { HeaderMenu } from '../../components/HeaderMenu'
+import { ReadyContainer } from '../../components/ReadyContainer'
+export function Support() {
   return (
     <div id='container-support'>
       <HeaderMenu />
       <div className='questions-support'>
         <div className='content-quertions-support'>
-        teste
+          <h2>Suporte</h2>
+          <h1>Perguntas Frequentes</h1>
+          <p>Nós sabemos que o PayMee é um sistema repleto de funcionalidades e que<br /> dúvidas vão surgir. Pensando em você,  criamos um FAQ completo para te ajudar!</p>
+          <div className='input-search'>
+            <img src={SearchIcon} alt="Pesquisar" />
+            <input type="text" placeholder='Procurar' />
+          </div>
+          <div className='email-support'>
+            <span>Não encontrou sua dúvida? </span>
+            <span><img src={EmailIcon} alt="email" /> suporte@paymee.com.br</span>
+          </div>
         </div>
       </div>
+      <div id='verifications'>
+        <div className='content-verification'>
+
+          <div className='part-one'>
+            <div className='verification first'>
+              <h2>Verificação</h2>
+              <ul>
+                <li><a href="#">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</a> </li>
+                <li><a href="#">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</a> </li>
+                <li><a href="#">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</a> </li>
+                <li><a href="#">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</a> </li>
+                <li><a href="#">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</a> </li>
+                <li><a href="#">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</a> </li>
+              </ul>
+            </div>
+
+            <div className='verification two'>
+              <h2>Verificação</h2>
+              <ul>
+                <li><a href="#">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</a> </li>
+                <li><a href="#">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</a> </li>
+                <li><a href="#">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</a> </li>
+                <li><a href="#">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</a> </li>
+                <li><a href="#">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</a> </li>
+                <li><a href="#">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</a> </li>
+              </ul>
+            </div>
+          </div>
+
+          <div className='part-two'>
+            <div className='verification three'>
+              <h2>Verificação</h2>
+              <ul>
+                <li><a href="#">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</a> </li>
+                <li><a href="#">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</a> </li>
+                <li><a href="#">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</a> </li>
+                <li><a href="#">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</a> </li>
+                <li><a href="#">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</a> </li>
+                <li><a href="#">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</a> </li>
+              </ul>
+            </div>
+
+            <div className='verification four'>
+              <h2>Verificação</h2>
+              <ul>
+                <li><a href="#">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</a> </li>
+                <li><a href="#">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</a> </li>
+                <li><a href="#">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</a> </li>
+                <li><a href="#">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</a> </li>
+                <li><a href="#">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</a> </li>
+                <li><a href="#">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</a> </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+
+      </div>
+      <div className='ready'>
+        <ReadyContainer />
+      </div>
+     
+      <FooterContainer />
     </div>
-    
+
   )
 }
