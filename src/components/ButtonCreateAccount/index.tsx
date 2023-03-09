7d88c380e43f7d24bdb154ebcee926df15c29807
Felipe Santana <felipe.jesus0801@gmail.com>
2023-03-09 17:09:02 -0300

    updated screen button

--- src/components/ButtonCreateAccount/index.tsx
+++ src/components/ButtonCreateAccount/index.tsx
@@ -11,6 +11,8 @@ export function ButtonCreateAccount({type}:ButtonCreateAccountProps){
   let color = type === "orange" ? "#FF481C" : "#1E1F35";
   
   return(
-    <a href="/create-account" className='button-create-account' style={{background:color}}> <span>Criar Conta</span> ➜</a>
+    <a href="/create-account" className='button-create-account' style={{background:color}}> 
+      <span>Criar Conta</span><span className='flash'>➜</span> 
+    </a>
   )
 }
